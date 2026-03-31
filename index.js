9d64950d7250f62a5becc90c12e81a9db15876fc
tjsn-00 <qor0628@korea.ac.kr>
2026-03-31 22:17:28 +0900

    week1 - animal sound API server
    https://www.youtube.com/watch?v=Tt_tKhhhJqY
    00:00 ~ 50:20

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+app.get('/sound/:name', (req, res) => {
+    const { name } = req.params
+    console.log(name)
+
+    if (name == 'dog') {
+        res.json({'sound': '멍멍'})
+    } else if (name == 'cat') {
+        res.json({'sound': '야옹'})
+    } else if (name == 'pig') {
+        res.json({'sound': '꿀꿀'})
+    } else {
+        res.json({'sound': '알 수 없음'})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
